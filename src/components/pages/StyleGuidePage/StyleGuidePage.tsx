9db87dbfda2cc2f4581e43f24e5cce4e31274e9f
Bartek0074 <bartekdo123@gmail.com>
2026-09-23 14:50:58 +0200

    chore: small change

--- src/components/pages/StyleGuidePage/StyleGuidePage.tsx
+++ src/components/pages/StyleGuidePage/StyleGuidePage.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 
 import { IconPlus, IconMinus, IconShoppingBag } from '@tabler/icons-react';
 
-import { MenuButton } from '@/src/components/layout/Navigation/components';
+import { MenuButton } from '@/src/components/layout/components';
 
 type StyleGuidePageProps = {};
 
